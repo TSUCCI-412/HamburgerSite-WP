6654e3e9aeaf1c81ff3b55ccf1f1e5a1cdd09d82
TSUCCI-412 <82869219+TSUCCI-412@users.noreply.github.com>
2022-09-10 12:21:22 +0900

    ページ404　フッター修正

--- 404.php
+++ 404.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
-<main class="l-main p-main">
-        <section class="p-main--404page">
+    <main class="l-main p-nf">
+        <section class="p-nf--404page">
             <h2> <?php _e('404 Not Found', 'HamburgerSite'); ?></h2>
-            <p class="p-main--404page__text">指定された以下のページは存在しないか、または移動した可能性があります。</p>
+            <p class="p-nf--404page__text">指定されたサイトは存在しないか、または移動した可能性があります。</p>
             <!--ページurl取得-->
-            <p class="p-main--404page__url">URL:<span><?php echo get_pagenum_link(); ?></span></p>
+            <p class="p-nf--404page__url">URL:<span><?php echo get_pagenum_link(); ?></span></p>
             <!--ホームへ戻る-->
-            <p class="p-main--404page__home">URLをご確認の上再読み込みするか、<a href="<?php echo esc_url(home_url());?>">トップページへ</a>お戻りください。</p>
+            <p class="p-nf--404page__home">URLをご確認の上、再読込みするか、<a href="<?php echo esc_url(home_url());?>">トップページへ</a>お戻りください。</p>
         </section>  
     </main>
 
