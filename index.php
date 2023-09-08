ccef8b30c5972fb349b87426bd7a57eafadd5427
TSUCCI-412 <82869219+TSUCCI-412@users.noreply.github.com>
2023-09-08 21:46:06 +0900

    本文変更

--- index.php
+++ index.php
@@ -1,96 +1,88 @@
 <?php get_header(); ?>
-            <main class="l-main">
-                <!--メインヴィジュアルのsection-->
-                <section class="p-mainvisual">
-                    <h2 class="p-mainvisual__title">ダミーサイト</h2>
-                </section>
+<main class="l-main">
+    <!--メインヴィジュアルのsection-->
+    <section class="p-mainvisual">
+        <h2 class="p-mainvisual__title">ハンバーガーショップのダミーサイト</h2>
+    </section>
 
-                <!--TakeoutとEatinメニューを囲む-->
-                <section class="l-main__wrapper p-branchmenu">
-                
-                <!--Takeoutのカテゴリーを取得してページ遷移させる-->
-                <div class="p-branchmenu--takeout">
-                <?php       
-                $cat = get_cat_ID('Take Out'); //カテゴリー名からカテゴリーIDを取得
-                $cat_link = get_category_link($cat);//取り出したカテゴリーIDをget_category_linkに指定
+    <!--TakeoutとEatinメニューを囲む-->
+    <section class="l-main__wrapper p-branchmenu">
+
+        <!--Takeoutのカテゴリーを取得してページ遷移させる-->
+        <div class="p-branchmenu--takeout">
+            <?php
+            $cat = get_cat_ID('Take Out'); //カテゴリー名からカテゴリーIDを取得
+            $cat_link = get_category_link($cat); //取り出したカテゴリーIDをget_category_linkに指定
             ?>
-                        <h3 class="p-branchmenu__head"><a href="<?php echo $cat_link ?>">Take Out</a></h3>
-                        <!--カードを囲む-->
-                        <ul class="p-branchmenu__list">
-                           <a href="<?php echo $cat_link ?>">
-                           <!--dlにリンクを付けるためにaタグを追加-->
-                            <li class="p-branchmenu__item">
-                                <dl>
-                                    <dt>Take Out</dt>
-                                    <dd>当店のテイクアウトで利用できる商品を掲載しています</dd>
-                                </dl>
-                            </li>
-                            <li class="p-branchmenu__item">
-                                <dl>
-                                    <dt>Take Out</dt>
-                                    <dd>当店のテイクアウトで利用できる商品を掲載しています</dd>
-                                </dl>
-                            </li>
-</a>
-                        </ul>
-                    </div>
-                    <!--Eatinのカテゴリーを取得してページ遷移させる-->
-                    <div class="p-branchmenu--eatin">
-                    <?php       
-                    $cat = get_cat_ID('Eat In');
-                    $cat_link = get_category_link($cat);
-                ?>
-                        <h3 class="p-branchmenu__head"><a href="<?php echo $cat_link ?>">Eat In</a></h3>
-                        <!--カードを囲む-->
-                        <ul class="p-branchmenu__list">
-                        <a href="<?php echo $cat_link ?>">
-                            <li class="p-branchmenu__item">
-                                <dl>
-                                    <dt>Eat In</dt>
-                                    <dd>当店のテイクアウトで利用できる商品を掲載しています</dd>
-                                </dl>
-                            </li>
-                            <li class="p-branchmenu__item">
-                                <dl>
-                                    <dt>Eat In</dt>
-                                    <dd>当店のテイクアウトで利用できる商品を掲載しています</dd>
-                                </dl>
-                            </li></a>
-                        </ul>
-                    </div>
-                    
-                </section>
-                <!--TakeoutとEatinメニューを囲むsection-->
-
-                <!--Mapのsection-->
-                <section class="l-main__map p-map">
-                    <div class="p-map__wrap">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13046.008691578849!2d136.87702724578173!3d35.169034399033535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600376e794d78b89%3A0x81f7204bf8261663!2z5ZCN5Y-k5bGL6aeF!5e0!3m2!1sja!2sjp!4v1631351592153!5m2!1sja!2sjp"
-                            style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                    </div>
-
-                    <!--地図のグレーカラー-->
-                    <div class="p-map__layer"></div>
-                    <div class="p-map__content">
-                        <h3 class="p-map__head">見出しが入ります</h3>
-                        <!--疑似要素で横線追加-->
-                        <p class="p-map__txt">
-                            テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                            テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                            テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                            テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                            テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                            テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                            テキストが入ります。テキストが入ります。テキストが入ります。
-                        </p>
-                    </div>
-
-                </section>
-            </main>
+            <h3 class="p-branchmenu__head"><a href="<?php echo $cat_link ?>">Take Out</a></h3>
+            <!--カードを囲む-->
+            <ul class="p-branchmenu__list">
+                <a href="<?php echo $cat_link ?>">
+                    <!--dlにリンクを付けるためにaタグを追加-->
+                    <li class="p-branchmenu__item">
+                        <dl>
+                            <dt>Take Out</dt>
+                            <dd>当店のテイクアウトで利用できる商品を掲載しています</dd>
+                        </dl>
+                    </li>
+                    <li class="p-branchmenu__item">
+                        <dl>
+                            <dt>Take Out</dt>
+                            <dd>当店のテイクアウトで利用できる商品を掲載しています</dd>
+                        </dl>
+                    </li>
+                </a>
+            </ul>
         </div>
-        <!--c-container__main-->
-        <?php get_sidebar(); ?>
+        <!--Eatinのカテゴリーを取得してページ遷移させる-->
+        <div class="p-branchmenu--eatin">
+            <?php
+            $cat = get_cat_ID('Eat In');
+            $cat_link = get_category_link($cat);
+            ?>
+            <h3 class="p-branchmenu__head"><a href="<?php echo $cat_link ?>">Eat In</a></h3>
+            <!--カードを囲む-->
+            <ul class="p-branchmenu__list">
+                <a href="<?php echo $cat_link ?>">
+                    <li class="p-branchmenu__item">
+                        <dl>
+                            <dt>Eat In</dt>
+                            <dd>当店のテイクアウトで利用できる商品を掲載しています</dd>
+                        </dl>
+                    </li>
+                    <li class="p-branchmenu__item">
+                        <dl>
+                            <dt>Eat In</dt>
+                            <dd>当店のテイクアウトで利用できる商品を掲載しています</dd>
+                        </dl>
+                    </li>
+                </a>
+            </ul>
+        </div>
 
-        <?php get_footer(); ?>
-                    
+    </section>
+    <!--TakeoutとEatinメニューを囲むsection-->
+
+    <!--Mapのsection-->
+    <section class="l-main__map p-map">
+        <div class="p-map__wrap">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13046.008691578849!2d136.87702724578173!3d35.169034399033535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!%3A0x81f7204bf8261663!2z5ZCN5Y-k5bGL6aeF!5e0!3m2!1sja!2sjp!4v1631351592153!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+
+        <!--地図のグレーカラー-->
+        <div class="p-map__layer"></div>
+        <div class="p-map__content">
+            <h3 class="p-map__head">お店の地図</h3>
+            <!--疑似要素で横線追加-->
+            <p class="p-map__txt">
+                当店は〇〇〇駅の△番出口を出て、南のドラックストア方面に向かって３分進んでいただき、左にコンビニエンスストアが見えたら左折してください。お店はすぐ右手にあります。お車でお越しの場合、駐車場はありませんが当店の北側にコインパーキングはありますのでそちらのご利用をお願い致します。テイクアウトの際はお時間を指定いただければ出来るだけお時間通りにご準備させて頂きます。
+            </p>
+        </div>
+
+    </section>
+</main>
+</div>
+<!--c-container__main-->
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
